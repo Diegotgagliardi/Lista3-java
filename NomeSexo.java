0912a7d1d50c112ee79513a99d8585aec930ae49
Diego Gagliardi <diegotgagliardi@gmail.com>
2024-04-01 18:14:48 -0300

    Exercicio 11

---
+++ NomeSexo.java
@@ -0,0 +1,17 @@
+public class NomeSexo {
+    private String nome;
+    private char sexo;
+
+    public NomeSexo(String nome, char sexo) {
+        this.nome = nome;
+        this.sexo = sexo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+}
