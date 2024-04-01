573980b55c705141e8efed9d59f89b05804e2958
Diego Gagliardi <diegotgagliardi@gmail.com>
2024-04-01 19:32:13 -0300

    Exercicio 05

---
+++ Exercicio05.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Exercicio05 {
+  public static void executar() {
+    Scanner leitor = new Scanner(System.in);
+    Prestacao prestacao = new Prestacao();
+
+    System.out.println("Insira o nome do produto: ");
+    String nome = leitor.nextLine();
+    prestacao.setNome(nome);
+
+    System.out.println("Insira o valor do produto: ");
+    double valor = leitor.nextDouble();
+    prestacao.setValor(valor);
+
+    System.out.println("\nNome do produto: " + prestacao.getNome() + "\nValor total: " + prestacao.getValor()
+        + "\nValor em 5 parcelas: " + prestacao.parcelas());
+
+    leitor.close();
+  }
+}
---
+++ Prestacao.java
@@ -0,0 +1,27 @@
+public class Prestacao {
+  public String nomeProd;
+  public double valor;
+  public double parcelas = 5;
+
+  public String getNome() {
+    return nomeProd;
+  }
+
+  public void setNome(String nomeProd) {
+    this.nomeProd = nomeProd;
+  }
+
+  public double getValor() {
+    return valor;
+  }
+
+  public void setValor(double valor) {
+    this.valor = valor;
+  }
+
+  public double parcelas() {
+    double valorParcelas = valor / parcelas;
+
+    return valorParcelas;
+  }
+}
