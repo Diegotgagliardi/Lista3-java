4ade3bf660b47c320da2fc7f5ad5debefa5a966a
Diego Gagliardi <diegotgagliardi@gmail.com>
2024-04-27 01:02:05 -0300

    ex11

---
+++ br/edu/up/exercicios/Exercicio11.java
@@ -0,0 +1,43 @@
+package br.edu.up.exercicios;
+import java.util.Scanner;
+
+import br.edu.up.modelos.NomeSexo;
+
+public class Exercicio11 {
+    public static void executar() {
+        Scanner scanner = new Scanner(System.in);
+        int totalHomens = 0;
+        int totalMulheres = 0;
+        
+        NomeSexo[] pessoas = new NomeSexo[56];
+        
+        for (int i = 0; i < pessoas.length; i++) {
+            System.out.println("Informe o nome da pessoa " + (i + 1) + ":");
+            String nome = scanner.nextLine();
+            
+            System.out.println("Informe o sexo da pessoa (M para masculino, F para feminino):");
+            char sexo = scanner.nextLine().toUpperCase().charAt(0);
+            
+            pessoas[i] = new NomeSexo(nome, sexo);
+            
+            if (sexo == 'M') {
+                totalHomens++;
+            } else if (sexo == 'F') {
+                totalMulheres++;
+            } else {
+                System.out.println("Sexo inválido. Por favor, informe 'M' para masculino ou 'F' para feminino.");
+                i--;
+            }
+        }
+        
+        System.out.println("\nInformações das pessoas:");
+        for (NomeSexo pessoa : pessoas) {
+            System.out.println("Nome: " + pessoa.getNome() + ", Sexo: " + (pessoa.getSexo() == 'M' ? "Masculino" : "Feminino"));
+        }
+        
+        System.out.println("Total de Homens: " + totalHomens);
+        System.out.println("Total de Mulheres: " + totalMulheres);
+        
+        scanner.close();
+    }
+}
