68fb5222897bc03a2e37f91c10b8afefe33e788b
Diego Gagliardi <diegotgagliardi@gmail.com>
2024-04-27 01:01:46 -0300

    ex9

---
+++ br/edu/up/exercicios/Exercicio09.java
@@ -0,0 +1,22 @@
+package br.edu.up.exercicios;
+import java.util.Scanner;
+
+import br.edu.up.modelos.Intervalo;
+
+public class Exercicio09 {
+    public static void executar() {
+        Scanner scanner = new Scanner(System.in);
+        
+        int[] numeros = new int[80];
+        for (int i = 0; i < 80; i++) {
+            System.out.print("Digite o " + (i + 1) + "º número: ");
+            numeros[i] = scanner.nextInt();
+        }
+
+        int quantidadeNoIntervalo = Intervalo.contarNumerosNoIntervalo(numeros);
+
+        System.out.println("Quantidade de números no intervalo entre 10 e 150: " + quantidadeNoIntervalo);
+
+        scanner.close();
+    }
+}
