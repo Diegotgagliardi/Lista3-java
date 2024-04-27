752e144cabca5db77b3c9e5c945d7dd3b9c04280
Diego Gagliardi <diegotgagliardi@gmail.com>
2024-04-27 01:02:55 -0300

    ex10

---
+++ br/edu/up/exercicios/Exercicio10.java
@@ -0,0 +1,31 @@
+package br.edu.up.exercicios;
+import java.util.Scanner;
+
+import br.edu.up.modelos.Pessoa;
+
+public class Exercicio10 {
+    public static void executar() {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Quantas pessoas você deseja verificar?");
+        int quantidadePessoas = scanner.nextInt();
+        
+        // Criar um array para armazenar as pessoas
+        Pessoa[] pessoas = new Pessoa[quantidadePessoas];
+        
+        // Preencher o array com as idades das pessoas
+        for (int i = 0; i < quantidadePessoas; i++) {
+            System.out.println("Informe a idade da pessoa " + (i+1) + ":");
+            int idade = scanner.nextInt();
+            pessoas[i] = new Pessoa(idade);
+        }
+        
+        // Mostrar o status de cada pessoa
+        System.out.println("\nStatus de cada pessoa:");
+        for (int i = 0; i < quantidadePessoas; i++) {
+            System.out.println("Pessoa " + (i+1) + " é " + pessoas[i].getStatusIdade());
+        }
+        
+        scanner.close();
+    }
+}
